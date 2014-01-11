acd4bb9bd06890c2b31f773d0657067be774c36a
somber73 <nytesblood@gmail.com>
2014-01-11 18:02:44 -0500

    Performance Bridge

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -625,6 +625,10 @@ public class Settings extends PreferenceActivity
                 if (!showDev) {
                     target.remove(i);
                 }
+            } else if (id == R.id.performance_controls) {
+                if (!showDev) {
+                    target.remove(i);
+                }				
             } else if (id == R.id.account_add) {
                 if (um.hasUserRestriction(UserManager.DISALLOW_MODIFY_ACCOUNTS)) {
                     target.remove(i);
@@ -1170,4 +1174,5 @@ public class Settings extends PreferenceActivity
     public static class QuickSettingsTilesSettingsActivity extends Settings { /* empty */ }
     public static class BatteryIconStyleSettingsActivity extends Settings { /* empty */ }
     public static class DisplayRotationSettingsActivity extends Settings { /* empty */ }
+	public static class PerformanceSettingsActivity extends Settings { /* empty */ }
 }
